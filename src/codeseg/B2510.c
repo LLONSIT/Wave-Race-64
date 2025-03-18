e22e2e826c244d3e6f6607b5068038048d3c0c4b
SonicDcer <alejandro.asenjo88@gmail.com>
2025-03-18 20:31:17 +0000

    turn overlay_A95D0 into a segment

---
+++ src/codeseg/B2510.c
@@ -0,0 +1,3 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/codeseg/B2510/func_A95D0_801E3EE0.s")
--- src/overlays/A95D0/ovl_B2510.c
+++
@@ -1,3 +0,0 @@
-#include "common.h"
-
-#pragma GLOBAL_ASM("asm/nonmatchings/overlays/A95D0/ovl_B2510/func_A95D0_801E3EE0.s")
