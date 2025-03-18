7e0ff6ada3b19b360a73fddb7c847076b2ad4f0f
SonicDcer <alejandro.asenjo88@gmail.com>
2025-03-18 07:11:18 +0000

    cleanup

--- include/macros.h
+++ include/macros.h
@@ -1,7 +1,6 @@
 #ifndef _MACROS_H_
 #define _MACROS_H_
 
-
 #ifndef __sgi
 #define GLOBAL_ASM(...)
 #endif
@@ -9,19 +8,19 @@
 #if !defined(__sgi) && (!defined(NON_MATCHING) || !defined(AVOID_UB))
 // asm-process isn't supported outside of IDO, and undefined behavior causes
 // crashes.
-//#error Matching build is only possible on IDO; please build with NON_MATCHING=1.
+// #error Matching build is only possible on IDO; please build with NON_MATCHING=1.
 #endif
 
 #define ARRAY_COUNT(arr) (s32)(sizeof(arr) / sizeof(arr[0]))
 
-#define GLUE(a, b) a ## b
+#define GLUE(a, b) a##b
 #define GLUE2(a, b) GLUE(a, b)
 
 // Avoid compiler warnings for unused variables
 #ifdef __GNUC__
 #define UNUSED __attribute__((unused))
 #else
-#define UNUSED //Ignore for IDO
+#define UNUSED // Ignore for IDO
 #endif
 
 // Avoid undefined behaviour for non-returning functions
@@ -53,13 +52,16 @@
 #endif
 
 // convert a virtual address to physical.
-#define VIRTUAL_TO_PHYSICAL(addr)   ((uintptr_t)(addr) & 0x1FFFFFFF)
+#define VIRTUAL_TO_PHYSICAL(addr) ((uintptr_t) (addr) & 0x1FFFFFFF)
 
 // convert a physical address to virtual.
-#define PHYSICAL_TO_VIRTUAL(addr)   ((uintptr_t)(addr) | 0x80000000)
+#define PHYSICAL_TO_VIRTUAL(addr) ((uintptr_t) (addr) | 0x80000000)
 
 // another way of converting virtual to physical
-#define VIRTUAL_TO_PHYSICAL2(addr)  ((u8 *)(addr) - 0x80000000U)
+#define VIRTUAL_TO_PHYSICAL2(addr) ((u8*) (addr) - 0x80000000U)
+
+// aligns an address to the next 2 bytes
+#define ALIGN_2(val) (((val) + 1) & ~1)
 
 // aligns an address to the next 16 bytes
 #define ALIGN16(val) (((val) + 0xF) & ~0xF)
