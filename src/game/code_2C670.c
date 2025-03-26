d96a6056654b91f58c10150a99ff208be98f7a32
SonicDcer <alejandro.asenjo88@gmail.com>
2025-03-26 07:28:56 +0000

    match func_80075274

--- src/game/code_2C670.c
+++ src/game/code_2C670.c
@@ -16,7 +16,41 @@
 
 #pragma GLOBAL_ASM("asm/nonmatchings/game/code_2C670/func_800744EC.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/game/code_2C670/func_80075274.s")
+void func_80071E70(void);
+void func_80073E6C(void);
+void func_800744EC(void);
+void func_80075310(void);
+void func_801FAEA8(void);
+extern s32 D_801AEE20;
+extern s32 D_801B2F20;
+extern s32 D_801B7020;
+extern s32 D_801BB120;
+extern s32 D_801BB124;
+extern s32 D_801BB128;
+extern s32 D_801CE638;
+
+void func_80075274(void) {
+    func_80071E70();
+    if (D_801CE638 != 21) {
+        func_800735EC(&D_801AEE20, D_801BB120);
+        if (D_801BB124 != 0) {
+            func_800735EC(&D_801B2F20, D_801BB124);
+        }
+        if (D_801BB128 != 0) {
+            func_800735EC(&D_801B7020, D_801BB128);
+        }
+        func_80073E6C();
+        func_800744EC();
+        func_80075310();
+        func_801FAEA8();
+    }
+}
+
+// These probably go inside func_80075310
+// (void)"dolphin condition full at goal\n";
+// (void)"dolphin condition full\n";
+// (void)"rank value adjust\n";
+// (void)"simultaneously goal !!\n";
 
 #pragma GLOBAL_ASM("asm/nonmatchings/game/code_2C670/func_80075310.s")
 
