4cbca66fe4bf96979a87a517ada074e3931e42bb
SonicDcer <alejandro.asenjo88@gmail.com>
2025-03-26 08:13:23 +0000

    bring over the progress script from the Starfox 64 Decompilation Project.

--- tools/progress.py
+++ tools/progress.py
@@ -1,89 +1,114 @@
-import argparse
-import os
-import re
-import sys
-import subprocess
-import glob
+#!/usr/bin/env python3
 
-def get_functions(elffile, section, ending=None):
+import argparse
+import git
+import os
+import subprocess
+import sys
+from colour import Color
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+root_dir = os.path.join(script_dir, "..")
+asm_dir = os.path.join(root_dir, "asm", "nonmatchings")
+build_dir = os.path.join(root_dir, "build")
+elf_path = os.path.join(build_dir, "waverace64.us.elf")
+
+def get_func_sizes():
     try:
-        result = subprocess.run(['objdump', '-x', elffile], stdout=subprocess.PIPE)
+        result = subprocess.run(['objdump', '-x', elf_path], stdout=subprocess.PIPE)
         nm_lines = result.stdout.decode().split("\n")
     except:
-        sys.stderr.write(f"Error: Could not run objdump on {elffile} - make sure that the project is built")
+        print(f"Error: Could not run objdump on {elf_path} - make sure that the project is built")
         sys.exit(1)
 
-    functions = {}
+    sizes = {}
+    total = 0
 
     for line in nm_lines:
-        if f"g     F {section}" in line:
+        if " F " in line:
             components = line.split()
             size = int(components[4], 16)
             name = components[5]
-            functions[name] = {"function": name, "length": size}
+            total += size
+            sizes[name] = size
 
-    return functions
+    return sizes, total
 
-def generate_csv(functions, nonmatching_funcs, version, section, subcode):
-    ret = []
-    ret.append("version,section,function,length,matching")
-    for func in functions:
-        length = functions[func]["length"]
-        if length > 0:
-            matching = "no" if func in nonmatching_funcs else "yes"
-            # Strip the boot_ prefix off symbols in bk_boot
-            func_name = func if subcode else func[5:]
-            ret.append(f"{version},{section},{func_name},{length},{matching}")
-    return "\n".join(ret)
+def get_nonmatching_funcs():
+    funcs = set()
+    for root, dirs, files in os.walk(asm_dir):
+        for f in files:
+            if f.endswith(".s"):
+                funcs.add(f[:-2])
 
-def get_nonmatching_funcs(basedir, subcode):
-    grepstr = r'#pragma GLOBAL_ASM\(.*/\K.*(?=\.s)'
-    if subcode:
-        try:
-            funcs = set(subprocess.check_output(['grep', '-ohPR', grepstr, basedir + '/src/' + subcode]).decode('ascii').split())
-        except subprocess.CalledProcessError as grepexc:
-            if grepexc.returncode != 1:
-                raise grepexc
-            funcs = set()
-    else:
-        args = ['grep', '-ohPs', '-d', 'skip', grepstr]
-        args.extend(glob.glob(basedir + '/src/*'))
-        try:
-            funcs = set(subprocess.check_output(args).decode('ascii').split())
-        except subprocess.CalledProcessError as grepexc:
-            if grepexc.returncode != 1:
-                raise grepexc
-            funcs = set()
-        try:
-            funcs = funcs.union(set(subprocess.check_output(['grep', '-ohPR', grepstr, basedir + '/src/done']).decode('ascii').split()))
-        except subprocess.CalledProcessError as grepexc:
-            if grepexc.returncode != 1:
-                raise grepexc
     return funcs
 
+def get_funcs_sizes(sizes, matchings, nonmatchings):
+    msize = 0
+    nmsize = 0
 
-def main(basedir, elffile, section, ending, version, subcode):
-    functions = get_functions(elffile, section, ending)
-    section_name = section.split("_")[-1] # .code_game -> game
-    nonmatching_funcs = get_nonmatching_funcs(basedir, subcode)
-    csv = generate_csv(functions, nonmatching_funcs, version, section_name, subcode)
-    print(csv)
+    for func in matchings:
+        msize += sizes[func]
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Create progress csv based on map file',
-                                     formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('basedir', type=str,
-                        help="base directory (containing src/)")
-    parser.add_argument('elffile', type=str,
-                        help=".elf file to be read")
-    parser.add_argument('section', type=str,
-                        help=".text section of the map")
-    parser.add_argument('--ending', type=str,
-                        help="section name that marks the end of 'section'")
-    parser.add_argument('--version', type=str, default='us',
-                        help="ROM version, us, eu, debug, ects")
-    parser.add_argument('--subcode', type=str, default=None,
-                        help="Subcode for section to get progress of")
+    for func in nonmatchings:
+        if func not in sizes:
+            pass
+            # print(func)
+        else:
+            nmsize += sizes[func]
+            #print("% s,%i" % (func, sizes[func]))
+
+    return msize, nmsize
+
+def lerp(a, b, alpha):
+    return a + (b - a) * alpha
+
+def main(args):
+    func_sizes, total_size = get_func_sizes()
+    all_funcs = set(func_sizes.keys())
+
+    nonmatching_funcs = get_nonmatching_funcs()
+    matching_funcs = all_funcs - nonmatching_funcs
+
+    matching_size, nonmatching_size = get_funcs_sizes(func_sizes, matching_funcs, nonmatching_funcs)
+
+    if len(all_funcs) == 0:
+        funcs_matching_ratio = 0.0
+        matching_ratio = 0.0
+    else:
+        funcs_matching_ratio = (len(matching_funcs) / len(all_funcs)) * 100
+        matching_ratio = (matching_size / total_size) * 100
+
+    if args.csv:
+        version = 1
+        git_object = git.Repo().head.object
+        timestamp = str(git_object.committed_date)
+        git_hash = git_object.hexsha
+        csv_list = [str(version), timestamp, git_hash, str(len(all_funcs)), str(len(nonmatching_funcs)),
+                    str(len(matching_funcs)), str(total_size), str(nonmatching_size), str(matching_size)]
+        print(",".join(csv_list))
+    elif args.shield_json:
+        import json
+
+        # https://shields.io/endpoint
+        color = Color("#50ca22", hue=lerp(0, 105/255, matching_ratio / 100))
+        print(json.dumps({
+            "schemaVersion": 1,
+            "label": f"progress",
+            "message": f"{matching_ratio:.2f}%",
+            "color": color.hex,
+        }))
+    else:
+        if matching_size + nonmatching_size != total_size:
+            print("Warning: category/total size mismatch!\n")
+        print(f"{len(matching_funcs)} matched functions / {len(all_funcs)} total ({funcs_matching_ratio:.2f}%)")
+        print(f"{matching_size} matching bytes / {total_size} total ({matching_ratio:.2f}%)")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Reports progress for the project")
+    parser.add_argument("--csv", action="store_true")
+    parser.add_argument("--shield-json", action="store_true")
     args = parser.parse_args()
 
-    main(args.basedir, args.elffile, args.section, args.ending, args.version, args.subcode)
+    main(args)
