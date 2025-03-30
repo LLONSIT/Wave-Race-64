d3389f71ceba7d5eaf4831b4507e711dbf0d808b
LLONSIT <lonsitsyt456@gmail.com>
2025-03-30 18:47:03 -0500

    Add wr64math.h

---
+++ include/wr64math.h
@@ -0,0 +1,13 @@
+#ifndef WR64MATH_H
+#define WR64MATH_H
+
+#define SIN(x) (gSinTable[(x) & 0xFFF])
+#define COS(x) (gSinTable[((x) + 0x400) & 0xFFF])
+#define TAN(x) (SIN(x) / COS(x))
+
+// Sin table
+extern float gSinTable[0x1000];
+
+void Math_Normalize_VectorComponents(f32* componentX, f32* componentY);
+
+#endif /* MATH_H */
