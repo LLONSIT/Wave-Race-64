ae1fe8ddecf29e882e29c5ae5e65f815ea7e3cc4
SonicDcer <alejandro.asenjo88@gmail.com>
2025-03-31 03:42:19 +0000

    B3AA0 funcs

--- src/codeseg/B3AA0.c
+++ src/codeseg/B3AA0.c
@@ -1,6 +1,39 @@
-#include "common.h"
+#include "global.h"
 
-#pragma GLOBAL_ASM("asm/nonmatchings/codeseg/B3AA0/func_801E5470.s")
+typedef struct UnkStruct_menu_buffer_1 {
+    u16 unk_0;
+    char pad_4[0x4];
+    u16 unk_6;
+    char pad_8[0x24];
+} UnkStruct_menu_buffer_1; // size 0x2C?
+
+extern UnkStruct_menu_buffer_1 menu_buffer_1[];
+
+void func_801E5470(void) {
+    UnkStruct_menu_buffer_1* var_s0;
+    s32 i;
+
+    for (i = 0; i != 5; i++) {
+        switch (menu_buffer_1[i].unk_0) {
+            case 1:
+                menu_buffer_1[i].unk_6 = 1;
+                func_801E6074(&menu_buffer_1[i]);
+                break;
+            case 2:
+                menu_buffer_1[i].unk_6 = 1;
+                func_801E62A8(&menu_buffer_1[i]);
+                break;
+            case 3:
+                menu_buffer_1[i].unk_6 = 1;
+                func_801E66F4(&menu_buffer_1[i]);
+                break;
+            case 4:
+                menu_buffer_1[i].unk_6 = 1;
+                func_801E68EC(&menu_buffer_1[i]);
+                break;
+        }
+    }
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/codeseg/B3AA0/func_801E5554.s")
 
@@ -14,4 +47,15 @@
 
 #pragma GLOBAL_ASM("asm/nonmatchings/codeseg/B3AA0/func_801E6A4C.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/codeseg/B3AA0/func_801E6F6C.s")
+s32 func_801E6F6C(void) {
+    s32 var_v0;
+    UnkStruct_menu_buffer_1* var_v1 = menu_buffer_1;
+
+    for (var_v0 = 0; var_v0 != 5; var_v0++) {
+        if (var_v1[var_v0].unk_0 == 0) {
+            break;
+        }
+    }
+
+    return var_v0 == 5 ? -1 : var_v0;
+}
