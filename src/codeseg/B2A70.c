03969a18da02262d6c0d0449a9280a4cfd7ab8fd
LLONSIT <lonsitsyt456@gmail.com>
2025-03-31 18:21:14 -0500

    Add some libultra files

--- src/codeseg/B2A70.c
+++ src/codeseg/B2A70.c
@@ -1,7 +1,22 @@
 #include "common.h"
 
+extern s16 D_801CE624;
+extern s16 D_801CE62C;
+extern s32 D_80228910;
+extern s32 gRiderGameModes;
+extern s16 D_801CE628;
+
 #pragma GLOBAL_ASM("asm/nonmatchings/codeseg/B2A70/func_801E4440.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/codeseg/B2A70/func_801E4C08.s")
+void func_801E4C08(void) {
+    D_801CE62C = 1;
+    D_801CE624 = 0;
+    D_80228910 = gRiderGameModes;
+    gRiderGameModes = 0;
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/codeseg/B2A70/func_801E4C38.s")
+void func_801E4C38(void) {
+    D_801CE624 = -1;
+    gRiderGameModes = D_80228910;
+    D_801CE628 = 0;
+}
--- src/os/osRecvMesg.c
+++ src/os/osRecvMesg.c
@@ -1,3 +1,30 @@
-#include "common.h"
+#include "libultra_internal.h"
 
-#pragma GLOBAL_ASM("asm/nonmatchings/os/osRecvMesg/osRecvMesg.s")
+s32 osRecvMesg(OSMesgQueue *mq, OSMesg *msg, s32 flag) {
+    register u32 int_disabled;
+    int_disabled = __osDisableInt();
+
+    while (!mq->validCount) {
+        if (!flag) {
+            __osRestoreInt(int_disabled);
+            return -1;
+        } else {
+            __osRunningThread->state = OS_STATE_WAITING;
+            __osEnqueueAndYield(&mq->mtqueue);
+        }
+    }
+
+    if (msg != NULL) {
+        *msg = *(mq->first + mq->msg);
+    }
+
+    mq->first = (mq->first + 1) % mq->msgCount;
+    mq->validCount--;
+
+    if (mq->fullqueue->next != NULL) {
+        osStartThread(__osPopThread(&mq->fullqueue));
+    }
+
+    __osRestoreInt(int_disabled);
+    return 0;
+}
