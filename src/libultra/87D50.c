28b4685715e5865744603ff149f3cdbd1b878bc5
SonicDcer <alejandro.asenjo88@gmail.com>
2025-04-04 22:30:33 +0000

    889A0

--- src/libultra/87D50.c
+++ src/libultra/87D50.c
@@ -15,13 +15,3 @@
 #pragma GLOBAL_ASM("asm/nonmatchings/libultra/87D50/__osPfsRWInode.s")
 
 #pragma GLOBAL_ASM("asm/nonmatchings/libultra/87D50/__osPfsSelectBank.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/87D50/func_800CE1A0.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/87D50/func_800CE78C.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/87D50/func_800CEA70.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/87D50/__osContRamRead.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/87D50/__osPackRamReadData.s")
---
+++ src/libultra/889A0.c
@@ -0,0 +1,11 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/889A0/func_800CE1A0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/889A0/func_800CE78C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/889A0/func_800CEA70.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/889A0/__osContRamRead.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/889A0/__osPackRamReadData.s")
