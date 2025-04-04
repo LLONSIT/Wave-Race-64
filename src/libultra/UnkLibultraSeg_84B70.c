7390396481d00db4b3d609d9c124af38758ae50b
SonicDcer <alejandro.asenjo88@gmail.com>
2025-04-04 22:06:59 +0000

    pidma (correct this time)

--- src/libultra/UnkLibultraSeg_84B70.c
+++
@@ -1,3 +0,0 @@
-#include "common.h"
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/UnkLibultraSeg_84B70/osPiStartDma.s")
--- src/libultra/pidma.c
+++ src/libultra/pidma.c
@@ -1,7 +1,3 @@
 #include "common.h"
 
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/pidma/func_800C85C0.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/pidma/func_800C8754.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/pidma/func_800C8834.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/pidma/osPiStartDma.s")
