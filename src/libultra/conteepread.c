f79a78e6e89efeed0187a1a4d27bcebe0b431efa
SonicDcer <alejandro.asenjo88@gmail.com>
2025-04-04 22:17:35 +0000

    conteepread.c

---
+++ src/libultra/conteepread.c
@@ -0,0 +1,5 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/conteepread/osEepromRead.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/conteepread/__osPackEepReadData.s")
--- src/libultra/unkLibultraSeg_87570.c
+++
@@ -1,5 +0,0 @@
-#include "common.h"
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/unkLibultraSeg_87570/osEepromRead.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/unkLibultraSeg_87570/__osPackEepReadData.s")
