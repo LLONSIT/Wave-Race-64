5e11671fd812fc49d710c3e43bc8d9691f5fda00
SonicDcer <alejandro.asenjo88@gmail.com>
2025-04-04 22:16:18 +0000

    pfsinit.c

--- src/libultra/UnkLibultraSeg_830F0.c
+++
@@ -1,5 +0,0 @@
-#include "common.h"
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/UnkLibultraSeg_830F0/osPfsInit.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/UnkLibultraSeg_830F0/__osPfsGetStatus.s")
---
+++ src/libultra/pfsinit.c
@@ -0,0 +1,5 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/pfsinit/osPfsInit.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/pfsinit/__osPfsGetStatus.s")
