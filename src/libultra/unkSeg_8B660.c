d57b3c2d29f4b078dc270d549f7abdce7a50a598
SonicDcer <alejandro.asenjo88@gmail.com>
2025-04-04 22:45:48 +0000

    crc.c

--- src/libultra/unkSeg_8B660.c
+++
@@ -1,5 +0,0 @@
-#include "common.h"
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/unkSeg_8B660/__osContAddressCrc.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/unkSeg_8B660/__osContDataCrc.s")
