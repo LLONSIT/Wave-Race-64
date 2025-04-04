87c73d5d8f756db7ffdda3945384e1aad96101b7
SonicDcer <alejandro.asenjo88@gmail.com>
2025-04-04 03:57:12 +0000

    func_80051538 + data

--- src/game/code_BD30.c
+++ src/game/code_BD30.c
@@ -1,43 +1,43 @@
-#include "common.h"
+#include "global.h"
 
-extern s32 D_1005748;
-extern s32 D_1006748;
-extern s32 D_800D47E0;
-extern s16 D_800D47E8;
-extern s16 D_800D47EC;
-extern s16 D_800D47F0;
-extern s16 D_800D47F4;
+extern u8 D_1005748[];
+extern Gfx D_1006748[];
+
+s32 D_800D47E0[2] = { 1, 2 };
+s16 gColorRed = 255;
+s16 gColorGreen = 255;
+s16 gColorBlue = 255;
+s16 gColorAlpha = 33;
+s16 D_800D47F8 = 255;
 
 void func_80051530(void) {
 }
 
-#ifdef DATA
-void* func_80051538(Gfx* gDisplayListHead) {
-    static u32 D_800D47FC;
+void* func_80051538(Gfx* gdl) {
+    static u32 D_800D47FC = { 0 };
 
-    if (D_800D47E0 == 0) {
-        return gDisplayListHead;
+    if (D_800D47E0[0] == 0) {
+        return gdl;
     }
 
-    gSPDisplayList(gDisplayListHead++, &D_1006748);
-    gDPLoadTextureBlock(gDisplayListHead++, &D_1005748, G_IM_FMT_I, G_IM_SIZ_8b, 64, 64, 0x0000,
-                        G_TX_NOMIRROR | G_TX_WRAP, G_TX_NOMIRROR | G_TX_WRAP, 6, 6, G_TX_NOLOD, G_TX_NOLOD);
-    gDPSetEnvColor(gDisplayListHead++, D_800D47E8, D_800D47EC, D_800D47F0, D_800D47F4);
+    gSPDisplayList(gdl++, D_1006748);
+    gDPLoadTextureBlock(gdl++, D_1005748, G_IM_FMT_I, G_IM_SIZ_8b, 64, 64, 0, G_TX_NOMIRROR | G_TX_WRAP,
+                        G_TX_NOMIRROR | G_TX_WRAP, 6, 6, G_TX_NOLOD, G_TX_NOLOD);
+    gDPSetEnvColor(gdl++, gColorRed, gColorGreen, gColorBlue, gColorAlpha);
 
-    if (D_800D47E0 == 1) {
-        gDPSetRenderMode(gDisplayListHead++,
+    if (D_800D47E0[0] == 1) {
+        gDPSetRenderMode(gdl++,
                          AA_EN | IM_RD | CVG_DST_WRAP | ZMODE_OPA | CVG_X_ALPHA |
                              GBL_c1(G_BL_CLR_MEM, G_BL_0, G_BL_CLR_MEM, G_BL_1MA),
                          AA_EN | IM_RD | CVG_DST_WRAP | ZMODE_OPA | CVG_X_ALPHA |
                              GBL_c2(G_BL_CLR_MEM, G_BL_0, G_BL_CLR_MEM, G_BL_1MA));
     } else {
-        gDPSetRenderMode(gDisplayListHead++, G_RM_CLD_SURF, G_RM_CLD_SURF2);
+        gDPSetRenderMode(gdl++, G_RM_CLD_SURF, G_RM_CLD_SURF2);
     }
+
     D_800D47FC = (D_800D47FC - 1) % 0x80;
-    gSPTextureRectangle(gDisplayListHead++, 0x0024, 0x0054, 0x04D8, 0x0368, G_TX_RENDERTILE, D_800D47FC * 32, 0, 0x0400,
-                        0x0400);
-    return gDisplayListHead;
+
+    gSPTextureRectangle(gdl++, 36, 84, 1240, 872, G_TX_RENDERTILE, D_800D47FC * 32, 0, 1024, 1024);
+
+    return gdl;
 }
-#else
-#pragma GLOBAL_ASM("asm/nonmatchings/game/code_BD30/func_80051538.s")
-#endif
