24f515db9adccf47b7552dacd76d1f5e37e450c8
SonicDcer <alejandro.asenjo88@gmail.com>
2025-04-04 22:03:21 +0000

    pidma

---
+++ src/libultra/pidma.c
@@ -0,0 +1,7 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/pidma/func_800C85C0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/pidma/func_800C8754.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/pidma/func_800C8834.s")
