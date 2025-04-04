5d60a3f2ecc5dc5e8c6cfb4672fdbaada341fc03
SonicDcer <alejandro.asenjo88@gmail.com>
2025-04-04 22:31:18 +0000

    contpfs.c

---
+++ src/libultra/contpfs.c
@@ -0,0 +1,17 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/contpfs/__osSumcalc.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/contpfs/__osIdCheckSum.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/contpfs/__osRepairPackId.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/contpfs/__osCheckPackId.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/contpfs/__osGetId.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/contpfs/__osCheckId.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/contpfs/__osPfsRWInode.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/contpfs/__osPfsSelectBank.s")
