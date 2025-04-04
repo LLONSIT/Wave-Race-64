9f4bbf29ec868b28662521e134f9e5dadb00cf35
SonicDcer <alejandro.asenjo88@gmail.com>
2025-04-04 22:39:59 +0000

    pfschecker.c

--- src/libultra/889A0.c
+++
@@ -1,7 +0,0 @@
-#include "common.h"
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/889A0/func_800CE1A0.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/889A0/func_800CE78C.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/889A0/func_800CEA70.s")
