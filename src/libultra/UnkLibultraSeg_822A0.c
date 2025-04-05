987db80120aec9e251d90081ed08ee439ee2ded2
SonicDcer <alejandro.asenjo88@gmail.com>
2025-04-05 05:32:14 +0000

    lookathil.c

--- src/libultra/UnkLibultraSeg_822A0.c
+++
@@ -1,5 +0,0 @@
-#include "common.h"
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/UnkLibultraSeg_822A0/func_800C7AA0.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/UnkLibultraSeg_822A0/func_800C820C.s")
---
+++ src/libultra/lookathil.c
@@ -0,0 +1,5 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/lookathil/guLookAtHiliteF.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/lookathil/guLookAtHilite.s")
