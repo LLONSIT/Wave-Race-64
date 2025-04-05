7ac666ee283d33d7b9ea862409294ec52b9371ce
SonicDcer <alejandro.asenjo88@gmail.com>
2025-04-05 01:21:49 +0000

    syncputchars & mtxutil

---
+++ src/libultra/mtxutil.c
@@ -0,0 +1,9 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/mtxutil/guMtxF2L.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/mtxutil/guMtxIdentF.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/mtxutil/guMtxIdent.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/mtxutil/guMtxL2F.s")
--- src/libultra/unkSeg_87300.c
+++
@@ -1,9 +0,0 @@
-#include "common.h"
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/unkSeg_87300/guMtxF2L.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/unkSeg_87300/guMtxIdentF.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/unkSeg_87300/func_800CCC88.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/unkSeg_87300/func_800CCCB8.s")
