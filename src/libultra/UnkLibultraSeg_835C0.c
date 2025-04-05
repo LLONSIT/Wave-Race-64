b7219686ebd113cf70ee96da657928011475519d
SonicDcer <alejandro.asenjo88@gmail.com>
2025-04-05 04:39:13 +0000

    pfsreadwritefile

--- src/libultra/UnkLibultraSeg_835C0.c
+++
@@ -1,5 +0,0 @@
-#include "common.h"
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/UnkLibultraSeg_835C0/func_800C8DC0.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/UnkLibultraSeg_835C0/func_800C8EA0.s")
