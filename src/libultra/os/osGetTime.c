2053d9ede136c5745e2f1df9901f3e8a07d9e3d6
SonicDcer <alejandro.asenjo88@gmail.com>
2025-04-29 21:14:55 +0000

    osGetTime

--- src/libultra/os/osGetTime.c
+++ src/libultra/os/osGetTime.c
@@ -1,3 +1,18 @@
-#include "common.h"
+#include "osint.h"
 
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/os/osGetTime/osGetTime.s")
+extern OSTime __osCurrentTime;
+extern u32 __osBaseCounter;
+
+OSTime osGetTime(void) {
+    u32 tmpTime;
+    u32 elapseCount;
+    OSTime currentCount;
+    register u32 saveMask;
+
+    saveMask = __osDisableInt();
+    tmpTime = osGetCount();
+    elapseCount = tmpTime - __osBaseCounter;
+    currentCount = __osCurrentTime;
+    __osRestoreInt(saveMask);
+    return currentCount + elapseCount;
+}
