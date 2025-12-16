58b3966212aeac7dfa53b514d910317c06d65f30
s <s>
2025-12-16 00:22:10 -0700

    merge master

---
+++ src/libultra/io/pfsdeletefile.c
@@ -0,0 +1,7 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/io/pfsdeletefile/osPfsDeleteFile.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/io/pfsdeletefile/__osPfsReleasePages.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/io/pfsdeletefile/__osBlockSum.s")
---
+++ src/libultra/io/pfsinit.c
@@ -0,0 +1,5 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/io/pfsinit/osPfsInit.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/io/pfsinit/__osPfsGetStatus.s")
