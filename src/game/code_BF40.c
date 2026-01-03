74842ac2efb21807a6925bf0a3ac239feaaf02cd
LLONSIT <lonsitsyt456@gmail.com>
2026-01-03 13:35:18 -0500

    Match code_BF40

--- src/game/code_BF40.c
+++ src/game/code_BF40.c
@@ -18,26 +18,12 @@ void func_80051740(struct UnkStruct_801C3C50* arg0, struct UnkStruct_8004B0F8* a
 // TODO: Improve this match
 void func_80051740(struct UnkStruct_801C3C50* arg0, struct UnkStruct_8004B0F8* arg1) {
     f32 temp_f0;
-    f32 temp_f0_2;
-    f32 temp_f10;
-    f32 temp_f12;
-    f32 temp_f14;
-    f32 temp_f22;
-    f32 temp_f24;
-    f32 temp_f26;
-    f32 temp_f28;
     f32 temp_f2;
-    f32 temp_f6;
     s16* var_s3;
     s32 temp_s1;
     s32 temp_s2;
-    s32 temp_v0;
-    s32 temp_v0_2;
-    s32 var_s2_2;
     struct UnkStruct_8004B0F8_1* test;
     struct UnkStruct_8004B0F8_1* var_v1;
-    f32 temp1;
-    f32 temp2;
     u8* var_s1;
     struct UnkStruct_8004B0F8_1* var_s0_2 = arg1->unk30;
     struct UnkStruct_8004B0F8_1* var_s0;
@@ -100,6 +86,85 @@ void func_80051740(struct UnkStruct_801C3C50* arg0, struct UnkStruct_8004B0F8* a
     }
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/game/code_BF40/func_80051A88.s")
+s32 func_80051A88(f32* arg0, f32* arg1) {
+    f32 temp_f12;
+    f32 temp_f20;
+    f32 temp_f2;
+    s16* var_a2;
+    s32 temp_v0;
+    s32 temp_v1;
+    s32 var_v1;
+    u8* var_a3;
+    struct UnkStruct_8004B0F8_1* temp_a2;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/game/code_BF40/func_80051CB4.s")
+    temp_v0 = (s32) *arg0 - D_801924F4;
+    if ((temp_v0 < 0) || (temp_v0 >= D_801924FC)) {
+        return 0;
+    }
+    temp_v1 = (s32) *arg1 - D_80192500;
+    if ((temp_v1 < 0) || (temp_v1 >= D_80192508)) {
+        return 0;
+    }
+
+    if (D_801924F0 != 0) {
+        var_a2 = &(0, D_80192530)[((temp_v1 >> D_80192528) | ((temp_v0 >> D_80192520) << D_80192518))];
+    } else {
+        var_a2 = &(0, D_80192530)[((temp_v0 >> D_80192520) | ((temp_v1 >> D_80192528) << D_80192510))];
+    }
+
+    var_v1 = 0;
+    var_a3 = D_80192534 + *var_a2;
+    while (*var_a3 != 0) {
+        temp_a2 = (*var_a3 + (0, D_80192538)); // FAKE
+        temp_f12 = ((temp_a2->unk0 - *arg0) * temp_a2->unk8) + ((temp_a2->unk4 - *arg1) * temp_a2->unkC);
+        if ((temp_f12 > 0.0f) && (temp_f12 < 32.0f)) {
+            temp_f20 = ((*arg0 - temp_a2->unk0) * temp_a2->unk10) + ((*arg1 - temp_a2->unk4) * temp_a2->unk14);
+            if ((temp_f20 > 0.0f) && (temp_f20 < temp_a2->unk18)) {
+                var_v1 = 1;
+                *arg0 += temp_f12 * temp_a2->unk8;
+                *arg1 += (temp_f12 * temp_a2->unkC);
+            }
+        }
+        var_a3++;
+    }
+    return var_v1;
+}
+
+f32 func_80051CB4(f32 arg0, f32 arg1, f32* arg2, f32* arg3) {
+    UnkStruct_8004B0F8_1* temp_a0;
+    f32 temp_f20;
+    f32 temp_f2;
+    s16* var_a0;
+    s32 temp_v0;
+    s32 temp_v1;
+    u8* var_v1;
+
+    temp_v0 = (s32) arg0 - D_801924F4;
+    if ((temp_v0 < 0) || (temp_v0 >= D_801924FC)) {
+        return 0.0f;
+    }
+    temp_v1 = (s32) arg1 - D_80192500;
+    if ((temp_v1 < 0) || (temp_v1 >= D_80192508)) {
+        return 0.0f;
+    }
+    if (D_801924F0 != 0) {
+        var_a0 = &(0, D_80192530)[((temp_v1 >> D_80192528) | ((temp_v0 >> D_80192520) << D_80192518))];
+    } else {
+        var_a0 = &(0, D_80192530)[((temp_v0 >> D_80192520) | ((temp_v1 >> D_80192528) << D_80192510))];
+    }
+    var_v1 = &D_80192534[*var_a0];
+    while (*var_v1 != 0) {
+        temp_a0 = (*var_v1 + (0, D_80192538)); // FAKE
+        temp_f2 = ((temp_a0->unk0 - arg0) * temp_a0->unk8) + ((temp_a0->unk4 - arg1) * temp_a0->unkC);
+        if ((temp_f2 > 0.0f) && (temp_f2 < 32.0f)) {
+            temp_f20 = ((arg0 - temp_a0->unk0) * temp_a0->unk10) + ((arg1 - temp_a0->unk4) * temp_a0->unk14);
+            if ((temp_f20 > 0.0f) && (temp_f20 < temp_a0->unk18)) {
+                *arg2 = temp_a0->unk8;
+                *arg3 = temp_a0->unkC;
+                return temp_f2;
+            }
+        }
+        var_v1++;
+    }
+    return 0.0f;
+}
