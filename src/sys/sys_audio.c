8b46b16202d6f38d6bea10ab8dc8268fd1bff95a
LLONSIT <lonsitsyt456@gmail.com>
2026-02-06 18:13:09 -0500

    Oops

--- src/sys/sys_audio.c
+++ src/sys/sys_audio.c
@@ -1,26 +1,13 @@
 #include "common.h"
 #include "wr64audio.h"
 
-<<<<<<< HEAD extern OSMesg sAudioTaskMsg;
-extern OSMesgQueue gAudioTaskMesgQueue;
-extern OSTask* gCurrentAudioTask;
-=======
-extern OSMesgQueue sAudioTaskMsg;
-extern OSMesg gAudioTaskMesgQueue;
-extern OSTask* gCurrentAudioOSTask;
->>>>>>> master
-
 void SysAudio_AudioThreadEntry(void* entry) {
     static OSTask* sAudioTask = NULL;
     AudioLoad_Init();
     while (true) {
         osRecvMesg(&gAudioTaskMesgQueue, &sAudioTaskMsg, 1);
         if (sAudioTask != 0) {
-<<<<<<< HEAD
-            gCurrentAudioTask = (s32) sAudioTask;
-=======
-            gCurrentAudioOSTask = (s32) sAudioTask;
->>>>>>> master
+            gCurrentGfxTask = (s32) sAudioTask;
             osSendMesg(&gMainThreadMesgQueue, (void*) 0x16, OS_MESG_NOBLOCK);
         }
 
