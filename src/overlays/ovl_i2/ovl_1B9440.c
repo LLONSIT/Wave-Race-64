15bc00990841d9fa0ea15e569fc81826c3a2930f
Siiiimon <simonjoecornelius@gmail.com>
2026-04-02 23:18:22 +0200

    match func_i2_802C5AE4 (co-authored-by: @LLONSIT)

--- src/overlays/ovl_i2/ovl_1B9440.c
+++ src/overlays/ovl_i2/ovl_1B9440.c
@@ -1,8 +1,31 @@
+#include "variables.h"
 #include "common.h"
+
+extern s32 D_802C8C58[2];
+extern s32 D_802C8C60[2];
+extern s16 D_802C8C68;
+extern s16 D_802C8C6A;
+
+s16 D_802C8C70[4];
 
 #pragma GLOBAL_ASM("asm/us/rev1/nonmatchings/overlays/ovl_i2/ovl_1B9440/func_i2_802C5800.s")
 
-#pragma GLOBAL_ASM("asm/us/rev1/nonmatchings/overlays/ovl_i2/ovl_1B9440/func_i2_802C5AE4.s")
+
+void func_i2_802C5AE4(void) {
+    s32 i;
+
+    for (i = 0; i < 2; i++) {
+        D_802C8C58[i] = 0;
+        D_802C8C60[i] = 0;
+        D_801CE6F4[i] = i;
+    };
+
+    for (i = 0; i < 4; i++) {
+        D_802C8C70[i] = 0;
+    }
+    D_802C8C68 = 0;
+    D_802C8C6A = 0;
+}
 
 #pragma GLOBAL_ASM("asm/us/rev1/nonmatchings/overlays/ovl_i2/ovl_1B9440/func_i2_802C5B4C.s")
 
